cb7977e9f5e450c54470a7d29606cff1021d3cf7
Alex <alexander.miller110@gmail.com>
2022-06-26 14:07:43 -0400

    Update to upload lambdas to same account S3
    Previous method wasn't working. This works every time. Also made a handful of settings global for simplicity.

--- main.py
+++ main.py
@@ -3,26 +3,58 @@ import os
 from dotenv import load_dotenv
 import json
 from cfn_flip import to_yaml, to_json
+import requests
 
 load_dotenv()
 
-client = boto3.client(
+directory = './zip_files/'
+bucket = 'project-loading-amm'
+remove_function_name = False
+
+lambda_client = boto3.client(
     "lambda",
+    aws_access_key_id=os.environ["aws_access_key_id"],
+    aws_secret_access_key=os.environ["aws_secret_access_key"]
+)
+
+s3_client = boto3.resource(
+    "s3",
     aws_access_key_id=os.environ["aws_access_key_id"],
     aws_secret_access_key=os.environ["aws_secret_access_key"]
 )
 
 
 def get_function_code_url(function_name):
-    response = client.get_function(
+    response = lambda_client.get_function(
         FunctionName=function_name,
     )
     return response["Code"]["Location"]
 
 
+def download_url(url, save_path, chunk_size=128):
+    r = requests.get(url, stream=True)
+    with open(save_path, 'wb') as fd:
+        for chunk in r.iter_content(chunk_size=chunk_size):
+            fd.write(chunk)
+
+
+def clear_directory(directory):
+    for file in os.listdir(directory):
+        os.remove(os.path.join(directory, file))
+
+
+def upload_file_to_s3(path, bucket_name, file_name):
+    try:
+        s3_client.meta.client.upload_file(path, bucket_name, file_name)
+        return True
+    except Exception as exp:
+        print('exp: ', exp)
+        return False
+
+
 def open_yml():
-    f = open("formation.yml", "r")
-    yaml_file = f.read()
+    file = open("formation.yml", "r")
+    yaml_file = file.read()
     some_json = to_json(yaml_file)
     return json.loads(some_json)
 
@@ -35,12 +67,12 @@ def get_yml_function_names(yaml_json):
     return function_names
 
 
-def update_s3_uri(yaml_json, function_codes):
+def update_s3_uri(yaml_json):
     for item in yaml_json["Resources"].keys():
         if yaml_json["Resources"][item]["Type"] == "AWS::Lambda::Function":
-            uri_code = function_codes[yaml_json["Resources"][item]["Properties"]["FunctionName"]]
             zip_file_update = {
-                "ZipFile" : uri_code
+                "S3Bucket": bucket,
+                "S3Key": yaml_json["Resources"][item]["Properties"]["FunctionName"] + '.zip'
             }
             yaml_json["Resources"][item]["Properties"]["Code"] = zip_file_update
     return yaml_json
@@ -67,14 +99,27 @@ function_codes = {}
 for name in function_names:
     function_codes[name] = get_function_code_url(name)
 
-# 3. Update URI locations
+# 3. Download zip files for each function
+clear_directory(directory)
+zip_files = []
+for function_name in function_codes.keys():
+    url = function_codes[function_name]
+    download_url(url, directory + function_name + '.zip')
+    zip_files.append(directory + function_name + '.zip')
+
+# 4. Upload to S3
+for zip_file in zip_files:
+    upload_file_to_s3(zip_file, bucket, zip_file.replace(directory, ''))
+
+# 5. Update Bucket locations
 yaml_file = update_s3_uri(yaml_json, function_codes)
 
-# 4. Remove Function Names to prevent duplicate errors
-yaml_file = remove_function_names(yaml_file)
+# 6. Remove Function Names to prevent duplicate errors
+if remove_function_name:
+    yaml_file = remove_function_names(yaml_file)
 
-# 5. Put new CloudFormation
-yaml_file = to_yaml(json.dumps(yaml_file, indent=4))
+# 7. Create new CloudFormation
+yaml_file = to_yaml(json.dumps(yaml_file, indent=4), clean_up=True)
 f = open("formation_updated.yml", "w")
 f.write(yaml_file)
 f.close()
